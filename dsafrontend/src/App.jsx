a8c88eb7ff521d81dab442b1626922b23836e183
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-03 22:28:36 +0800

    Frontend React DSA Project

---
+++ dsafrontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const url = "http://localhost:8000/products";
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((responce) => {
+        setProducts(responce.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Mrs. G Menu! From Laravel</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.product_name}: {product.product_price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,11 @@
+To Do List:
+
+Finalize Databse Structure
+
+code the frontend where to display the data
+
+make mockup data for the database atleast 10 minmum spread out.
+
+pass the data from laravel to react but making sure only taking within a month (input one data that is sold on november to test if its taken)
+
+go from there...(charts/percentages of most sold/etc)
