2139a755eea946ce4881e78763d9a8755872042d
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-04 21:26:23 +0800

    Updated to Tailwind CSS and Added React Fonts Library

--- dsafrontend/src/App.jsx
+++ dsafrontend/src/App.jsx
@@ -1,31 +1,25 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import Sidebar from "./sidebar/sidebar";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const url = "http://localhost:8000/api/products";
-  useEffect(() => {
-    axios
-      .get(url)
-      .then((responce) => {
-        setProducts(responce.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  // const [products, setProducts] = useState([]);
+  // const url = "http://localhost:8000/api/products";
+  // useEffect(() => {
+  //   axios
+  //     .get(url)
+  //     .then((responce) => {
+  //       setProducts(responce.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
 
   return (
-    <div className="App">
-      <h1>Mrs. G Menu! From Laravel</h1>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.product_name}: {product.product_price}
-          </li>
-        ))}
-      </ul>
+    <div className="flex">
+      <Sidebar />
     </div>
   );
 }
---
+++ dsafrontend/src/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+  return (
+    <div className=" w-1/5 p-12">
+      <ul className="space-y-4">
+        <li className="cursor-pointer">Dashboard</li>
+        <li>Products</li>
+        <li>Estimated Incomes</li>
+        <li>Customers</li>
+      </ul>
+    </div>
+  );
+}
