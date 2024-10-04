522789610d6ed8f676392590c8259aba4d34bd74
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-04 21:43:36 +0800

    SideBar Finished

--- dsafrontend/src/sidebar/Sidebar.jsx
+++ dsafrontend/src/sidebar/Sidebar.jsx
@@ -1,11 +1,28 @@
+import { FaHome } from "react-icons/fa";
+import { BsBox } from "react-icons/bs";
+import { MdOutlineAttachMoney } from "react-icons/md";
+import { FaPeopleGroup } from "react-icons/fa6";
+
 export default function Sidebar() {
   return (
-    <div className=" w-1/5 p-12">
-      <ul className="space-y-4">
-        <li className="cursor-pointer">Dashboard</li>
-        <li>Products</li>
-        <li>Estimated Incomes</li>
-        <li>Customers</li>
+    <div className="w-1/5 pl-12 pt-12">
+      <ul className="space-y-4 cursor-pointer w-full">
+        <li className="flex  p-4 hover:bg-gray-200 transition duration-300 rounded-lg ">
+          <FaHome className="scale-150 mr-4" />
+          Dashboard
+        </li>
+        <li className="flex p-4 hover:bg-gray-200 transition duration-300 rounded-lg">
+          <BsBox className="scale-150 mr-4" />
+          Products
+        </li>
+        <li className="flex p-4 hover:bg-gray-200 transition duration-300 rounded-lg">
+          <MdOutlineAttachMoney className="scale-150 mr-4" />
+          Estimated Incomes
+        </li>
+        <li className="flex p-4 hover:bg-gray-200 transition duration-300 rounded-lg">
+          <FaPeopleGroup className="scale-150 mr-4" />
+          Customers
+        </li>
       </ul>
     </div>
   );
