dddc22d52993782d255703fe37c2271b74f3f41c
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-05 16:41:18 +0800

    Beautiful Chart from ReChart Added

--- dsafrontend/src/dashboard/Dashboard.jsx
+++ dsafrontend/src/dashboard/Dashboard.jsx
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+// import { VictoryChart, VictoryTheme, VictoryLine } from "victory";
 
 export default function Dashboard() {
   const url = "http://localhost:8000/api/sales";
@@ -33,10 +42,25 @@ export default function Dashboard() {
       setItemsSold(counter);
     });
   }
+
+  const salesByDate = salesTable.reduce((acc, sale) => {
+    const date = sale.sale_date;
+    const totalitems = parseFloat(sale.quantity);
+
+    if (!acc[date]) {
+      acc[date] = { date, sold: 0 };
+    }
+    acc[date].sold += totalitems;
+
+    return acc;
+  }, []);
+
+  const resultArray = Object.values(salesByDate);
+
   return (
     <div>
       <h1 className="text-[2.5vw] font-bold">Dashboard</h1>
-      <span className="text-gray-600 mt-4">Last 30 Days</span>
+      <span className="text-gray-600 mt-4">Last 28 Days</span>
       <h3 className="mt-4 mb-4 font-bold">Total Sales</h3>
       <div className="flex gap-4">
         <div className="flex-grow p-4 border-solid border-2 border-gray-300 rounded-lg">
@@ -48,8 +72,24 @@ export default function Dashboard() {
           <h1 className="font-bold text-[1.5vw]">₱{grossSales}</h1>
         </div>
       </div>
-      <div>random dump area for testing:</div>
-      <button onClick={() => countQuantity()}>Log Table</button>
+      <div className="mt-8 ">
+        <h1 className="mb-4 font-bold text-[1.5vw]">Sales Over Time</h1>
+        <LineChart
+          width={1200}
+          height={500}
+          data={resultArray}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="sold" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </div>
+      <button onClick={() => console.log(salesTable)}>Log Table</button>
     </div>
   );
 }
+
+//ctrl alt L for turbo console.log
