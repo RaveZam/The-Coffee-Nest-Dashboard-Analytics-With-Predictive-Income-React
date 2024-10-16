6120477388d67e086838314be664e6fc4814825f
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-16 14:03:14 +0800

    Images Added

--- dsafrontend/src/Point of Sales/PointofSalesMainHub.jsx
+++ dsafrontend/src/Point of Sales/PointofSalesMainHub.jsx
@@ -9,7 +9,8 @@ export default function PointOfSalesMainJub() {
         {products.map((product) => (
           <div key={product.id} className="w-1/6 bg-gray-500">
             <h1>{product.product_name} </h1>
-            <img src="" alt="" />
+            <img src={product.img_url} alt="" />
+            <span>Stocks{product.stock}</span>
             <span>{product.product_price}</span>
           </div>
         ))}
